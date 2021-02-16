ec0df4c1335b2cb641671c7729b59522dfc1501b
DavidMcMorrow <mcmorrda@tcd.ie>
2021-02-16 21:02:24 +0000

    worked out how to set up the vehilce models and made some basic setup files

--- Roadworks/RoadworksTMS.py
+++ Roadworks/RoadworksTMS.py
@@ -1,12 +1,15 @@
 import os
+import numpy as np
 import sys
 import optparse
 from sumolib import checkBinary  # Checks for the binary in environ vars
 import traci
+import matplotlib.pyplot as plt
 
 def runRoadWorksTMS():
     print("RoadWorks")
     step = 0
+    
     while traci.simulation.getMinExpectedNumber() > 0:
         traci.simulationStep()
         print(step)
