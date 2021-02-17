711d707e1a1b9cbb37c807b34bcbc1b412297445
DavidMcMorrow <mcmorrda@tcd.ie>
2021-02-17 19:49:05 +0000

    sorted the add files in collision

--- Collision/CollisionTMS.py
+++ Collision/CollisionTMS.py
@@ -10,7 +10,7 @@ def runCollisionTMS():
     while traci.simulation.getMinExpectedNumber() > 0:
         traci.simulationStep()
         print(step)
-        if step == 10:
+        if step == 100:
             traci.vehicle.setStop("crashed-car-lane-zero.0", "left-exit", 25, 0, 1000)
             traci.vehicle.setStop("crashed-car-lane-zero.1", "left-exit", 20, 0, 1000)
             traci.vehicle.setStop("crashed-car-lane-one.0", "left-exit", 25, 1, 1000)
